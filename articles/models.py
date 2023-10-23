53136e1c29583253599cc34596b89c94c3d85e71
Abdullah098Nadeem <39324092+Abdullah098Nadeem@users.noreply.github.com>
2023-10-23 13:00:47 +0500

    Changed articles model (#2)
    * Added fields to articles model.

--- articles/models.py
+++ articles/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    publish_at = models.DateField(
+        auto_now=False, auto_now_add=False, null=True, blank=True)
